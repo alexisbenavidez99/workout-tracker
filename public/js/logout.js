6d24d428e303de58590bc7f464fe3abbdb4c4ca0
Nathaniel Motley <nmotley92@gmail.com>
2023-01-25 21:24:59 -0600

    fix typo

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,19 +1,4 @@
 const logout = async () => {
-<<<<<<< HEAD
-    const response = await fetch('/api/users/logout', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-    });
-  
-    if (response.ok) {
-      document.location.replace('/login');
-    } else {
-      alert(response.statusText);
-    }
-  };
-  
-  document.querySelector('#logout').addEventListener('click', logout);
-=======
   const response = await fetch('/api/users/logout', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
@@ -25,4 +10,5 @@ const logout = async () => {
     alert(response.statusText);
   }
 };
->>>>>>> 26568d9710609647eaa00b8857332d6968454437
+
+document.querySelector('#logout').addEventListener('click', logout);

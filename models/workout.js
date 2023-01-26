590c5da068170671cabd41cadad597d31ee6648f
Nathaniel Motley <nmotley92@gmail.com>
2023-01-25 20:48:15 -0600

    adds workout model

---
+++ models/workout.js
@@ -0,0 +1,47 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Workout extends Model {}
+
+Workout.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    type: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    date: {
+        type: DataTypes.DATE,
+        allowNull: false
+    },
+    duration: {
+        type: DataTypes.INTEGER,
+        allowNull: true
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id'
+      }
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'workout',
+  }
+);
+
+module.exports = Workout;
--- public/js/logout.js
+++ public/js/logout.js
@@ -1,14 +1,21 @@
-const logout = async () => {
-    const response = await fetch('/api/users/logout', {
+//  logout function
+const logout = async (req, res) => {
+  const response = await fetch('/api/users/logout', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-    });
-  
-    if (response.ok) {
-      document.location.replace('/login');
-    } else {
-      alert(response.statusText);
-    }
-  };
-  
-  document.querySelector('#logout').addEventListener('click', logout);
+  });
+  if (response.ok) {
+      document.location.replace('/');
+  } else {
+      alert("Failed to logout");
+  }
+
+  if (req.session.loggedIn) {
+      req.session.destroy(() => {
+          res.status(204).end();
+      });
+  } else {
+      res.status(404).end();
+  }
+};
+document.querySelector('#logout').addEventListener('click', logout);

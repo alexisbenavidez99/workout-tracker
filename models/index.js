4ef06d812ab58afcbb5aeb721382e0939094c89c
Nathaniel Motley <nmotley92@gmail.com>
2023-01-26 19:22:29 -0600

    hompage route works

--- models/index.js
+++ models/index.js
@@ -1,13 +1,14 @@
 const User = require('./User');
+const Workout = require('./Workout');
 // add associations
 
 User.hasMany(Workout, {
-    foreignKey: 'user_id',
-    onDelete: 'CASCADE'
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
 });
 
-Workout.belongsTo(User, {   
-    foreignKey: 'user_id'
+Workout.belongsTo(User, {
+  foreignKey: 'user_id'
 });
 
 

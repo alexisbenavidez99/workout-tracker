e8e35522ae3a48f36481836504fbe521dd41be1e
alexisbenavidez99 <callmealexis49@gmail.com>
2023-01-26 21:30:38 -0600

    fixed exercise api route

--- public/js/exercises.js
+++ public/js/exercises.js
@@ -1,12 +1,15 @@
 let muscle = 'biceps';
-fetch({
+$.ajax({
   method: 'GET',
   url: 'https://api.api-ninjas.com/v1/exercises?muscle=' + muscle,
-  headers: { 'X-Api-Key': 'Y+nxTPsyrkSvnAOH9CKTkQ==dnVOTmqx3Q0WpvJT'},
-  contentType: 'application/json'
-})
-  .then(function (data) {
-    console.log(data);
-  });
+  headers: { 'X-Api-Key': 'Y+nxTPsyrkSvnAOH9CKTkQ==dnVOTmqx3Q0WpvJT' },
+  contentType: 'application/json',
+  success: function (result) {
+    console.log(result);
+  },
+  error: function ajaxError(jqXHR) {
+    console.error('Error: ', jqXHR.responseText);
+  },
+});
 
 
